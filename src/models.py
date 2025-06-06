c3d76e2ba187d91bbaed85bf3b492785b91a742e
fl-armable <fl.irlanda@gmail.com>
2025-06-06 17:15:49 +0000

    Done

--- src/models.py
+++ src/models.py
@@ -1,19 +1,96 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Enum, Date, Time, DateTime, Integer, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
+class Base(DeclarativeBase):
+  pass
 
-class User(db.Model):
+db = SQLAlchemy(model_class=Base)
+
+class User(Base):
+
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    firstname: Mapped[str] = mapped_column(String(50), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(50), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname
         }
+    
+class Follower(Base):
+
+    __tablename__ = "follower"
+
+    id: Mapped[int] = mapped_column(primary_key=True)    
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "user_from_id": self.user_from_id,
+            "user_to_id": self.user_to_id
+        }
+
+class Post(Base):
+
+    __tablename__ = "post"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+        }
+    
+class Media(Base):
+
+    __tablename__ = "media"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+    type: Mapped[str] = mapped_column (nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "type": self.type,
+            "url": self.url
+        }
+    
+class Comment(Base):
+
+    __tablename__ = "comment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'), nullable=False)
+    authr_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    comment_text: Mapped[str] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "post_id": self.post_id,
+            "author_id": self.authr_id,
+            "comment_text": self.comment_text
+        }
+
+try:
+    render_er(Base, 'diagram.png')
+    print("✅ Diagrama generado correctamente como diagram.png")
+except Exception as e:
+    print("❌ Error generando el diagrama:", e)
